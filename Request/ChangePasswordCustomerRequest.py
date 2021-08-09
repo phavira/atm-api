fc3d366bd0d2b1acbe1299eb4817eab9422cfd63
phavira <phavira@gmail.com>
2021-08-09 21:56:40 +0700

    modify token

--- Request/ChangePasswordCustomerRequest.py
+++ Request/ChangePasswordCustomerRequest.py
@@ -14,19 +14,20 @@ class ChangePasswordCustomer:
         def check(*args, **kwargs):
             try:
                 data = {}
+
                 if request.args.get('current_password') is None:
                     data["current_password"] = "The current_password is required."
+
                 if request.args.get("new_password") is None:
                     data['new_password'] = "The new password is required."
                 if request.args.get("confirm_password") is None:
                     data['confirm_password'] = "The confirm_password is required."
-
-                if (request.args.get('confirm_password') != request.args.get('new_password')):
+                elif (request.args.get('confirm_password') != request.args.get('new_password')):
                     data['confirm_password'] = "The confirm password and new password not match"
-
-                check = bcrypt.checkpw(request.args.get('current_password').encode(), auth.user.password.encode())
-                if not check:
-                    data['current_password'] = "The confirm password and new password not match"
+                else:
+                    check = bcrypt.checkpw(request.args.get('current_password').encode(), auth.user.password.encode())
+                    if not check:
+                        data['current_password'] = "The confirm password and new password not match"
 
                 if len(data) > 0:
                     raise ErrorMessage("The data is Invalid", 403, data)
